ee6e2543fb28257a90478b2426e80000347a3ea9
Alvi <alviarash620@gmail.com>
2024-02-29 18:08:00 +0600

    toke repo

--- com.inn.cafe/src/main/java/com/inn/cafe/repository/UserRepository.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.inn.cafe.repository;
 
 import com.inn.cafe.POJO.User;
 import com.inn.cafe.wrapper.UserWrapper;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -21,6 +23,10 @@ String updateQuery= "update user u  " +
         "set u.status=%:status% " +
         "where u.id=%:id%";
 
+
+
+   @Transactional
+   @Modifying
    @Query(value =updateQuery,nativeQuery = true)
    Integer updateStatus(@Param("status") String status,@Param("id") Integer id);
 //    Optional<User> findByUsername(String username);
--- com.inn.cafe/src/main/java/com/inn/cafe/service/UserService.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/service/UserService.java
@@ -3,6 +3,8 @@ package com.inn.cafe.service;
 
 import com.inn.cafe.wrapper.UserWrapper;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -16,6 +18,7 @@ public interface UserService {
 
 
 ResponseEntity<List<UserWrapper>>getAllUser();
+
 
 ResponseEntity<String>updateSatus(Map<String,String>request);
 
